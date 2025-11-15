c116503bd30faba328602bcb9fd78fd3309e5e3c
maguilarsilva <mariana.sistemas@gmail.com>
2025-11-14 20:52:29 -0500

    Add GameMinDTO, GameRepository, GameService e GameController.

---
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+//registrar ele dentro do sistema. Sendo um componente do sistema (@component ou @service).
+@Service
+public class GameService { //chama o repository e retonar dto
+
+    @Autowired     //Injetar GameRepositoy
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+       List<Game> result =  gameRepository.findAll();
+       // List<GameMinDTO>dtos = result.stream().map(game -> new GameMinDTO(game)).toList();
+        return result.stream().map(GameMinDTO::new).toList();
+    }
+
+
+}
