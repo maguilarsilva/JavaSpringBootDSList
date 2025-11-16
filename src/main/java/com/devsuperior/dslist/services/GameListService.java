cfbe2230b83a173238780c9f701cd83cd0b595e8
maguilarsilva <mariana.sistemas@gmail.com>
2025-11-15 19:58:28 -0500

    Add API que busca listas de games

---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslist.services;
+
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService { //chama o repository e retonar dto
+
+    @Autowired     //Injetar GameRepositoy
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+       List<GameList> result =  gameListRepository.findAll();
+        return result.stream().map(GameListDTO::new).toList();
+    }
+
+}
